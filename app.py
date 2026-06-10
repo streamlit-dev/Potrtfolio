d4eefcc078e7997a5ccd703c051e33e7fad7ef03
suman <suman59kkr@gmail.com>
2026-06-10 11:22:08 +0530

    Update imports for langchain community modules

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import os
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
